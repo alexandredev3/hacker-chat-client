3fd589363a6a60db80f1ea4db7ec3200fd414c43
alexandredev3 <costasalexandre511@gmail.com>
2021-03-23 20:34:06 -0300

    feature: added connection with the server

--- index.js
+++ index.js
@@ -1,22 +1,17 @@
 // chamar todos os modulos do nosso sistema
 
-/**
-  node index.js \
-    --username alexandredev3 \
-    --room sala01 \
-    --hostUri localhost
-*/
-
 import Events from 'events';
+
 import { TerminalController } from './src/terminalController.js';
 import { CliConfig } from './src/cliConfig.js';
+import { SocketClient } from './src/socket.js';
 
 const [nodePath, filePath, ...commands] = process.argv;
 const config = CliConfig.parseArguments(commands);
 
-console.log(config);
-
 const componentEmitter = new Events();
+const socketClient = new SocketClient(config);
 
+await socketClient.initialize();
 // const controller = new TerminalController();
 // await controller.initializeTable(componentEmitter);
---
+++ src/socket.js
@@ -0,0 +1,41 @@
+export class SocketClient {
+  #serverConnection = {};
+
+  constructor({ host, port, protocol }) {
+    this.host = host;
+    this.port = port;
+    this.protocol = protocol;
+  }
+
+  async createConnection() {
+    const options = {
+      port: this.port,
+      host: this.host,
+      headers: {
+        // temos que enviar um header parecido com que o server espera.
+        Connection: 'Upgrade',
+        Upgrade: 'websocket' // um upgrate de protocolo
+      }
+    }
+  
+    const http = await import(this.protocol);
+    const req = http.request(options);
+    req.end();
+  
+    return new Promise((resolve) => {
+
+      // podemos so executar uma promise apenas uma vez.
+      // se esse evento for acontecer varias vezes.
+      // nos vamos peder as proximas vezes.
+      // então vamos usar o once para escutar so uma vez.
+      req.once('upgrade', (res, socket) => resolve(socket));
+    });
+  }
+
+  async initialize() {
+    // populando a conexão;
+    this.#serverConnection = await this.createConnection();
+
+    console.log('I connected to the server!');
+  }
+}
